1fbbf7daf176437eeeca4acc0412752d1cfca558
Edin Dudojević <edudojevic@users.noreply.github.com>
2020-12-08 10:33:39 +0100

    add stats script

---
+++ stats/README.md
@@ -0,0 +1,2 @@
+# Install (Python 3)
+pip3 install -r requirements.txt
---
+++ stats/stats.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import urllib.request
+import json
+ 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+JSON_URL = os.getenv('JSON_URL')
+SESSION = os.getenv('SESSION')
+
+DAY = '8'
+
+req = urllib.request.Request(JSON_URL, None, { 'Cookie': 'session=' + SESSION }, method='GET')
+
+def getUserCompletionDay(ms, mid, DAY):
+   stars = ms[mid]['completion_day_level']
+   firstDayTimestamp = int(stars[DAY]['1']['get_star_ts'])
+   secondDayTimestamp = int(stars[DAY]['2']['get_star_ts']) if '2' in stars[DAY].keys() else 0
+
+   return [ms[mid]['name']] + [firstDayTimestamp] + [secondDayTimestamp]
+
+with urllib.request.urlopen(req) as response:
+   json_response = response.read()
+   json = json.loads(json_response)
+
+   ms = json['members']
+   completed = [getUserCompletionDay(ms, mid, DAY) for mid in ms if DAY in ms[mid]['completion_day_level'].keys()]
+
+   completedSortedStarOne = sorted(completed, key=lambda tup: tup[1])
+   completedSortedStarTwo = filter(lambda x: x[2] > 0, sorted(completed, key=lambda tup: tup[2]))
+
+   print(f'=== DAY {DAY} ===')
+   print('--- STAR 1 ---')
+   [print(datetime.fromtimestamp(x[1]), x[0]) for x in completedSortedStarOne]
+   print('\n')
+   print('--- STAR 2 ---')
+   [print(datetime.fromtimestamp(x[2]), x[0]) for x in completedSortedStarTwo]
